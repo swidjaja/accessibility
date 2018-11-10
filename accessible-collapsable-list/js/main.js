e1172934b03d8e3eadd08d5ab165adc8fba3026b
sugi <widjaja.s@gmail.com>
2018-11-09 23:58:27 -0800

    Make ul scrollable + only update selected element on enter

--- accessible-collapsable-list/js/main.js
+++ accessible-collapsable-list/js/main.js
@@ -17,55 +17,101 @@ window.sw.a11y.CollapsableList = {
     RIGHT_ARROW: 39,
     DOWN_ARROW: 40
   },
+  selectedItem: null,
 
   init(toggleButtonEl, itemsListEl) {
     this._toggleButtonEl = toggleButtonEl;
     this._itemsListEl = itemsListEl;
 
-    this.setToggleButtonLabel();
-    this.focusOnActiveDescendant();
+    this.initSelectedItem();
+    this.setItemToFocus();
 
     this.setupListeners();
   },
 
-  getSelectedOption() {
-    const itemsList = this._itemsListEl;
-    const activeDescendant = itemsList.getAttribute('aria-activedescendant');
-    const activeElement = itemsList.querySelector('#' + activeDescendant);
-
-    return {
-      id: activeElement.id,
-      value: activeElement.innerHTML
-    };
-  },
-
-  setToggleButtonLabel() {
+  initSelectedItem() {
     const itemsList = this._itemsListEl;
     const itemsListOptions = itemsList.querySelectorAll('li[role="option"]');
 
-    this._toggleButtonEl.innerHTML = itemsListOptions[0].innerHTML;
+    this.selectedItem = itemsListOptions[0];
+    this.selectedItem.classList.add('selected');
+    this.selectedItem.setAttribute('aria-selected', 'true');
+    this.setToggleButtonLabel(this.selectedItem);
+  },
+
+  setItemToFocus() {
+    const itemsList = this._itemsListEl;
+  
+    itemsList.setAttribute('aria-activedescendant', this.selectedItem.id);
+    this.selectedItem.classList.add('focused');
+  },
+
+  getSelectedItemValue() {
+    return (this.selectedItem && this.selectedItem.innerHTML) || '';
+  },
+
+  getActiveDescendant() {
+    const itemsList = this._itemsListEl;
+    const activeDescendant = itemsList.getAttribute('aria-activedescendant');
+
+    return itemsList.querySelector('#' + activeDescendant);
+  },
+
+  setToggleButtonLabel(selectedItem) {
+    this._toggleButtonEl.innerHTML = selectedItem.innerHTML;
   },
 
   focusOnActiveDescendant() {
-    const itemsList = this._itemsListEl;
-    const activeDescendant = itemsList.getAttribute('aria-activedescendant');
-    const activeElement = itemsList.querySelector('#' + activeDescendant);
+    const activeElement = this.getActiveDescendant();
 
     activeElement.classList.add('focused');
   },
 
-  updateSelectedEl(activeElement, nextActiveElement) {
+  updateFocusedEl(activeElement, nextActiveElement) {
     if (activeElement && nextActiveElement) {
       const itemsList = this._itemsListEl;
       const toggleButton = this._toggleButtonEl;
 
       activeElement.classList.remove('focused');
-      activeElement.removeAttribute('aria-selected');
       nextActiveElement.classList.add('focused');
-      nextActiveElement.setAttribute('aria-selected', true);
 
       itemsList.setAttribute('aria-activedescendant', nextActiveElement.id);
-      toggleButton.innerHTML = nextActiveElement.innerHTML;
+    }
+  },
+
+  updateSelectedItem() {
+    const activeElement = this.getActiveDescendant();
+
+    this.selectedItem.classList.remove('selected');
+    this.selectedItem.removeAttribute('aria-selected');
+  
+    this.selectedItem = activeElement;
+  
+    this.selectedItem.classList.add('selected');
+    this.selectedItem.setAttribute('aria-selected', 'true');
+  
+    this.setToggleButtonLabel(this.selectedItem);
+
+    console.log('Current selected item - ', this.getSelectedItemValue());
+  },
+
+  updateScrollPosition() {
+    const itemsList = this._itemsListEl;
+    const activeDescendant = itemsList.getAttribute('aria-activedescendant');
+    const activeElement = itemsList.querySelector('#' + activeDescendant);
+    const itemsListHeight = itemsList.clientHeight;
+    const itemsListScrollTop = itemsList.scrollTop;
+    const activeElementOffsetTop = activeElement.offsetTop;
+
+    if (itemsList.scrollHeight > itemsListHeight) {
+      const scrollBottom = itemsListHeight + itemsListScrollTop;
+      const elementBottom = activeElementOffsetTop + activeElement.offsetHeight;
+      if (elementBottom > scrollBottom) {
+        itemsList.scrollTop = elementBottom - itemsListHeight;
+      }
+      else if (activeElement.offsetTop < itemsListScrollTop) {
+        itemsList.scrollTop = activeElementOffsetTop;
+      }
     }
   },
 
@@ -76,6 +122,7 @@ window.sw.a11y.CollapsableList = {
     toggleButton.addEventListener('click', () => {
       const isExpanded = toggleButton.getAttribute('aria-expanded');
       if (!isExpanded || isExpanded === 'false') {
+        this.setItemToFocus();
         toggleButton.setAttribute('aria-expanded', true);
         itemsList.classList.remove('hide');
         itemsList.setAttribute('tabindex', -1);
@@ -92,19 +139,22 @@ window.sw.a11y.CollapsableList = {
       const activeDescendant = itemsList.getAttribute('aria-activedescendant');
       const activeElement = itemsList.querySelector('#' + activeDescendant);
 
-      if (keyCode === this._keyCodes.LEFT_ARROW || keyCode === this._keyCodes.UP_ARROW) {
+      if (keyCode === this._keyCodes.UP_ARROW || keyCode === this._keyCodes.DOWN_ARROW) {
         event.preventDefault();
-        const nextActiveElement = activeElement.previousElementSibling;
-        this.updateSelectedEl(activeElement, nextActiveElement);
-      } else if (keyCode === this._keyCodes.RIGHT_ARROW || keyCode === this._keyCodes.DOWN_ARROW) {
-        event.preventDefault();
-        const nextActiveElement = activeElement.nextElementSibling;
-        this.updateSelectedEl(activeElement, nextActiveElement);
+        const nextActiveElement = (keyCode === this._keyCodes.UP_ARROW) ?
+          activeElement.previousElementSibling :
+          activeElement.nextElementSibling;
+        this.updateFocusedEl(activeElement, nextActiveElement);
+        this.updateScrollPosition();
       } else if (keyCode === this._keyCodes.ENTER || keyCode === this._keyCodes.ESC) {
         event.preventDefault();
         toggleButton.setAttribute('aria-expanded', false);
         itemsList.classList.add('hide');
         itemsList.removeAttribute('tabindex');
+
+        if (keyCode === this._keyCodes.ENTER) {
+          this.updateSelectedItem();
+        }
 
         // return focus to button
         toggleButton.focus();              
